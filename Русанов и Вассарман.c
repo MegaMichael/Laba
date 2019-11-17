f625ee12153de902680f70ad2836e8fb4d11ee5f
MegaMichael <55753140+MegaMichael@users.noreply.github.com>
2019-11-17 14:31:00 +0500

    Add files via upload

---
+++ Русанов и Вассарман.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a;
+	int i = 0;
+	while (scanf("%d", a)) {
+		printf("%d", a);
+		if (a == 5)
+			i += 1;
+		printf("%s", "число: ");
+		printf("%d\n", i);
+	}
+	return 0;
+}
